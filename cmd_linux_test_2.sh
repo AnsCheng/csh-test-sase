5baa899863bd722a360711a5d8b0c7d20cd6a933
AnsCheng <test_ans@163.com>
2024-10-12 10:58:46 +0800

    1058

--- cmd_linux_test_2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-time=$(date "+%Y-%m-%d %H:%M.%S")
-echo "[$time] Hello World!"
-echo "[$time] This is test for linux cloudshell!"
--- csh-test-c++1551.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "张乐 18729003333" << "\n";
-    return 0;
-}
