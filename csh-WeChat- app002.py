78b8453a8368b65600cf4b0f57d10aefdd9e8b6b
AnsCheng <test_ans@163.com>
2024-10-23 15:32:08 +0800

    333

--- csh-WeChat- app002.py
+++ csh-WeChat- app002.py
@@ -8,7 +8,7 @@ def run_applescript(script):
         result = subprocess.check_output(['osascript', '-e', script], universal_newlines=True)
         return result.strip()
     except subprocess.CalledProcessError as e:
-        print(f"Error running AppleScript: {e}")
+        print(f"Error running AppleScript: {e}")11111
         return "Unknown"
 
 
