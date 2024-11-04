534afb9eb42721d1dde2c74eda578d8c3c394870
AnsCheng <test_ans@163.com>
2024-11-04 15:19:50 +0800

    9090

--- csh-WeChat- app00211.py
+++ csh-WeChat- app00211.py
@@ -36,7 +36,7 @@ def install_wechat_from_app_store():
                 log "Search results loaded"
                 
                 -- 找到搜索结果列333表
-                set resultList to collection view 1 of scroll area 1
+                set resultList to colcscslection view 1 of scroll area 1
                 log "Result list found"
                 
                 -- 检查每个UI元素以找到下载按钮
