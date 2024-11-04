1eef4627c2af82fed4706408d88b111037de5bc6
AnsCheng <test_ans@163.com>
2024-11-04 15:23:28 +0800

    8880

--- csh-WeChat- app00211.py
+++ csh-WeChat- app00211.py
@@ -35,7 +35,7 @@ def install_wechat_from_app_store():
                 end repeat
                 log "Search results loaded"
                 
-                -- 找到搜索结果列33321113表
+                -- 找到搜索结果列33321sss113表
                 set resultList to colcscslection view 1 of scroll area 1
                 log "Result list found"
                 
